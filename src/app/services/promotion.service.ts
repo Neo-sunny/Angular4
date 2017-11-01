695c102fb0831c25841226842ecc084d674ae4a1
neo <neo.sunny2k13@gmail.com>
2017-11-01 20:10:17 +0530

    Single Page Applications Part 1

---
+++ src/app/services/promotion.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Promotion } from '../shared/promotion';
+import { PROMOTION } from '../shared/promotions';
+
+@Injectable()
+export class PromotionService {
+
+  constructor() { }
+
+  getPromotions(): Promotion[]{
+    return PROMOTION;
+  }
+  getPromotion(id: number): Promotion{
+    return PROMOTION.filter((promo) =>(promo.id === id) )[0];
+  }
+  getFeaturedPromotion() :Promotion{
+    return PROMOTION.filter((promotion) => promotion.featured)[0];
+  }
+
+}
